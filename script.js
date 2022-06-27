ac6e74ddc11931b166a2df5de5d637c26aaa7c4a
mastroczar <106982941+mastroczar@users.noreply.github.com>
2022-06-27 00:01:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+for(let cont = 20; cont >= 1; cont = cont - 1){
+    console.log(cont)
+}
+console.log("-----")
+
+for(let contPar = 1; contPar <= 20; contPar++){
+    if(contPar % 2 == 0)
+        console.log(contPar)
+}
+console.log("-----")
+
+for(let contImpar = 1; contImpar <= 20; contImpar++){
+    if(contImpar % 2 !== 0)
+        console.log(contImpar)
+}
+console.log("-----")
+
+
+for(let contMultiploDe5 = 1; contMultiploDe5 <= 100; contMultiploDe5++){
+    if(contMultiploDe5 % 5 == 0)
+        console.log(contMultiploDe5)
+}
+console.log("-----")
+
+for(let contQuadrado = 1; contQuadrado <= 10; contQuadrado++){
+    console.log(contQuadrado ** 2)
+}
+console.log("-----")
+
+for(let cont = 20; cont >= 1; cont = cont - 1){
+    console.log(cont)
+}
+console.log("-----")
+
+for(let contPar = 20; contPar >= 1; contPar = contPar - 1){
+    if(contPar % 2 == 0)
+        console.log(contPar)
+}
+console.log("-----")
+
+for(let contImpar = 20; contImpar >= 1; contImpar = contImpar - 1){
+    if(contImpar % 2 !== 0)
+        console.log(contImpar)
+}
+console.log("-----")
+
+for(let contMultiploDe5 = 100; contMultiploDe5 >= 1 ; contMultiploDe5 = contMultiploDe5 - 1){ 
+    if(contMultiploDe5 % 5 == 0)
+        console.log(contMultiploDe5)
+}
+console.log("-----")
+
+for(let contQuadrado = 10; contQuadrado >= 1; contQuadrado = contQuadrado - 1){
+    console.log(contQuadrado ** 2)
+}
